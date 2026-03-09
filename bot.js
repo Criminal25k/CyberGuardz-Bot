77a494f3eb2e8393bbaa660aa8b691e5d27ce262
Criminal25k <albinjaison50@gmail.com>
2026-03-10 04:11:39 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const { default: makeWASocket } = require("@whiskeysockets/baileys")
+
 const scan = require("./commands/scan")
 const breach = require("./commands/breach")
 const password = require("./commands/password")
@@ -12,29 +13,36 @@ sock.ev.on("messages.upsert", async ({ messages }) => {
 
 const msg = messages[0]
 const text = msg.message?.conversation
+
 if(!text) return
 
 const jid = msg.key.remoteJid
 
 if(text === "/start"){
+
 await sock.sendMessage(jid,{
-text:`
-🛡 Cyber Security Bot
+text:`🔐 Cyber Security Bot
 
 Commands:
-
 /scan <url>
 /breach <email>
 /password
-/ip <ip>
-/whois <domain>
-/cve <keyword>
 /tip
-/news
 `
 })
+
 }
 
+if(text.startsWith("/scan")) scan(sock,jid,text)
+if(text.startsWith("/breach")) breach(sock,jid,text)
+if(text.startsWith("/password")) password(sock,jid)
+if(text.startsWith("/tip")) tips(sock,jid)
+
+})
+
+}
+
+startBot()
 if(text.startsWith("/scan")) scan(sock,jid,text)
 if(text.startsWith("/breach")) breach(sock,jid,text)
 if(text.startsWith("/password")) password(sock,jid)
